075640615a9ea1d63e1eacdd0547b7c2589b29bc
Erick Sanchez <esericksanc@gmail.com>
2022-03-26 22:51:07 -0700

    Initial Commit

---
+++ Goals-Causes and Effects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goals-Causes and Effects
+//
+//  Created by Erick Sanchez on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
